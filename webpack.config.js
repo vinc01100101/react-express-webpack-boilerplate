c7578c22fc7ccaea98e17b4f21ddccfd369c89c5
vincE <vrytoledo@gmail.com>
2021-03-01 15:28:50 +0800

    set webpack to use optimization.runtimeChunk='single'

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: {
-        home: ["./src_client/Home.js"],
-    },
-    output: {
-        path: path.join(__dirname, "dist"),
-        publicPath: "/js/",
-        filename: `[name].js`,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"],
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    resolve: {
-        alias: {},
-    },
-    plugins: [],
+	mode: "development",
+	entry: {
+		home: ["./src_client/Home.js"],
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		publicPath: "/js/",
+		filename: `[name].js`,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+						},
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		alias: {},
+	},
+	plugins: [],
+	optimization: {
+		runtimeChunk: "single",
+		minimize: false,
+	},
 };
